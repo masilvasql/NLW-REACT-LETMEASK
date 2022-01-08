8c6bf90240045e7f18f582b8eec3a08dbae04214
Marcelo Abrão da Silva <masilvasql@gmail.com>
2022-01-07 22:52:47 -0300

    Adicionado Estilização SASS, Criado componentes, criado tela inicial e tela de criação das salas, criado autenticação com o google - Firebase, criado hooks, criado contexto de autenticação, salvo no estados, tipagens

---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,43 @@
+import { useAuth } from '../hooks/useAuth';
+
+import {Link} from 'react-router-dom'
+
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+
+import { Button } from '../components/button';
+
+import '../styles/auth.scss';
+
+export function NewRoom(){
+
+  const {user} = useAuth();
+
+  return(
+    <div id = "page-auth">
+      <aside>
+        <img src={illustrationImg} alt="Ilustração Simbolizando perguntas e respostas" />
+        <strong>Crie Salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo-real</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask"/>
+          <h2>Criar uma nova sala</h2>
+          <form>
+            <input 
+              type="text" 
+              placeholder="Nome da Sala"
+            />
+            <Button type="submit"> 
+              Criar Sala
+            </Button>
+          </form>
+          <p>
+            Quer entrar em uma sala existente? <Link to="/">clique Aqui</Link>
+          </p> 
+        </div>
+      </main>
+    </div>
+  )
+}
